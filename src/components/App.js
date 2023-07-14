9fd2daff3915499db6db249600a56ce0f890d643
Hitesh <gaikwadhitesh03@gmail.com>
2023-07-14 19:13:49 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import CounterButton from '../components/CounterButton';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/* Do not remove the main div */
+        <CounterButton/>
+        }
     </div>
   )
 }
